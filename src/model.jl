288d0d65e0033f290ae5e83084c4244f640dc01f
adrienbanse <adrien.banse@gmail.com>
2023-10-26 16:10:02 +0200

    modify moments of inertia

--- src/model.jl
+++ src/model.jl
@@ -3,7 +3,7 @@ using LinearAlgebra
 # wheeled inverted pendulum parameters
 l = 23.0        # [mm]
 r = 40.0        # [mm]
-θ_r = 26.89     # [kg * mm^2]
+θ_r = 277.26    # [kg * mm^2]
 m_r = 0.316     # [kg]   
 θ_w = 444.43    # [kg * mm^2]
 m_w = 0.021     # [kg]
